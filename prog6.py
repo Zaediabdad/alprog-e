925fe0e5492304c2cb5a08ff651da777c9c79628
Zaediabdad <kingabdad389@gmail.com>
2024-11-10 05:31:33 -0800

    Add files via upload

---
+++ prog6.py
@@ -0,0 +1,25 @@
+# variabel assigment value
+x = 9
+y = 6
+print("nilai dari x adalah : ", y)
+
+# aturan penamaan fariabel
+luas = 30 # bisa berupa kata 
+Zaedi_Abdad= 100 # jika dua kata, digabung dg "_"
+Keziax = 10 # boleh gabung capital non-kapital
+keziax = 3
+lenca88 = 10 # jika menggabungkan huruf dan angka, angkanya di akhir
+print("nilai dar kezia adalah : ", lenca88 )
+
+
+a = 4 # integer bilangan bulat
+print("tipe data dari a : ", type(a))
+
+b = 6.9 # float bilangan pecahan
+print("tipe data dari b : ", type(b))
+
+c = "Gacor" # string, gabungan karakter
+print("tipe dsts dsri c : ", type(c))
+
+d = False # boolean, true false
+print("tipe dsri d : ", type(d))
